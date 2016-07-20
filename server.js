7da119c09038eb3cd89b27ce0053133c30ac89d3
Grzegorz Świdnicki <grzegorzswidnicki@MacBook-Pro-Grzegorz.local>
2016-07-20 19:09:30 +0200

    Update configuration

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + 'app/'));
+app.use(express.static(__dirname + '/app'));
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile('./app/index.html', {root: __dirname});
 });
 
